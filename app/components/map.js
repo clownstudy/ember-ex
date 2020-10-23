1eedc72f66582e1a9790fde6c2adaa6936e88135
clownstudy <angelos0424@gmail.com>
2020-10-23 17:08:38 +0900

    token에 접근하는 map.js 구현

--- app/components/map.js
+++ app/components/map.js
@@ -1,4 +1,11 @@
 import Component from '@glimmer/component';
+import ENV from 'super-rentals/config/environment';
 
+//MapComponent 내의 this.token(getter)에 접근.
+// token 접근 시 URL-encode는 중요함.
 export default class MapComponent extends Component {
+  get token(){
+    return encodeURIComponent(ENV.MAPBOX_ACCESS_TOKEN);
+  }
 }
+
