19078b3ee0e1b4044224d22290ba276d1f190d1e
clownstudy <angelos0424@gmail.com>
2020-10-23 19:40:20 +0900

    data를 route에서 가져오는 식으로 수정

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,6 @@
 import Route from '@ember/routing/route';
 
 export default class IndexRoute extends Route {
-  // model hook 하는 메서드
   async model() {
     return {
       title: 'Grand Old Mansion',
