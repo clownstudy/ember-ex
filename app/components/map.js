5dde022536d678d976a1dc2d4be0ab3b384e97ce
clownstudy <angelos0424@gmail.com>
2020-10-23 18:16:37 +0900

    map test까지 구현

--- app/components/map.js
+++ app/components/map.js
@@ -1,5 +1,5 @@
 import Component from '@glimmer/component';
-import ENV from 'super-rentals/config/environment';
+import ENV from 'ember-ex/config/environment';
 
 //MapComponent 내의 this.token(getter)에 접근.
 // token 접근 시 URL-encode는 중요함.
